b264794596f9df9c3d926d338a99f435f67833d8
MarioEliasER <201G0247@rcarbonifera.tecnm.mx>
2023-10-03 15:11:51 -0600

    cambios

--- Actividad1ZooPlanet/Controllers/EspeciesController.cs
+++ Actividad1ZooPlanet/Controllers/EspeciesController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Actividad1ZooPlanet.Models.Entities;
+using Actividad1ZooPlanet.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Actividad1ZooPlanet.Controllers
 {
     public class EspeciesController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string Id)
         {
-            return View();
+            AnimalesContext context = new AnimalesContext();
+            var clase = context.Clase.Where(x=> x.Nombre == Id).FirstOrDefault();
+            var datos = context.Especies.OrderBy(x => x.Especie).Where(x => x.IdClase == clase.Id).Select(x => new EspecieModel
+            {
+                Id = x.Id,
+                Nombre = x.Especie ?? ""
+            });
+            IndexEspeciesViewModel vm = new()
+            {
+                Id = clase.Id,
+                Especie = Id,
+                ListaEspecies = datos
+            };
+            return View(vm);
         }
     }
 }
---
+++ Actividad1ZooPlanet/Models/ViewModels/IndexEspeciesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Actividad1ZooPlanet.Models.ViewModels
+{
+    public class IndexEspeciesViewModel
+    {
+        public int Id { get; set; }
+        public string Especie { get; set; } = null!;
+        public IEnumerable<EspecieModel> ListaEspecies { get; set; } = null!;
+    }
+
+    public class EspecieModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
